8854874a066a87ef9d0188fe227aef4b07e8e1fa
Ksenia Yurko <ksyu257@gmail.com>
2024-06-21 00:13:52 +0300

    dynamic render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
-'use client';
+'use client'
 
-import styles from "./page.module.css"
-import MapView from "./components/MapViewPoints/MapViewPoints"
-import Calendar from "./components/Calendar/Calendar"
-
-
+import { useState } from 'react';
+import dynamic from 'next/dynamic';
+import styles from './page.module.css';
+import Calendar from './components/Calendar/Calendar';
 import { years, months, monthData } from './components/Calendar/calendarData';
 
+const DynamicMapViewPoints = dynamic(() => import('./components/MapViewPoints/MapViewPoints'), {
+  ssr: false
+});
+
 export default function Home() {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
   return (
     <main className={styles.main}>
-      <Calendar years={years} months={months} monthData={monthData}/>
-      <MapView />
+      <Calendar years={years} months={months} monthData={monthData} />
+      <DynamicMapViewPoints />
     </main>
   );
 }
--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -1,17 +1,22 @@
 'use client'
 
 import ReportsTable from "../components/TableComponent/TableComponent";
-import MapViewReports from "../components/MapViewReports/MapViewReports";
 import { useState } from "react";
+import dynamic from 'next/dynamic';
+
+// Динамический импорт компонента MapViewReports с отключением SSR
+const DynamicMapViewReports = dynamic(() => import('../components/MapViewReports/MapViewReports'), {
+  ssr: false // Отключаем серверный рендеринг для этого компонента
+});
 
 const ReportsPage: React.FC = () => {
   const [selectedCoordinates, setSelectedCoordinates] = useState<{ latitude: number, longitude: number } | null>(null);
+
   return (
     <>
-      <MapViewReports selectedCoordinates={selectedCoordinates}/>
+      <DynamicMapViewReports selectedCoordinates={selectedCoordinates}/>
       <ReportsTable setSelectedCoordinates={setSelectedCoordinates}/>
     </>
-    
   );
 };
 
